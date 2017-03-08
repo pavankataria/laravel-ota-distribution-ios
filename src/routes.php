b3b704c77576ec7c696c0c10bfb2285b170f0c79
Pavan Kataria <kataria.pavan@gmail.com>
2017-03-08 02:17:36 +0000

    Updates routes and composer json file to include carbon

--- src/routes.php
+++ src/routes.php
@@ -1,55 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pavankataria
- * Date: 07/03/2017
- * Time: 01:35
- */
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Input;
 
-Route::get('hello', function(){
-    echo "hello";
-});
-Route::post('/build', function () {
+Route::post('/api/build', function () {
     $buildUrlRoute = secure_url('api/build');
     $publicUrl = secure_url('/');
     $branch = Input::get('branch');
     $file = Input::file('file');
     $bundleVersionNumber = request()->input('build_version_number');
-
     echo "branch: {$branch}";
     echo "\nfile: {$file}";
     echo "\nsize of file: {$file->getSize()}";
     echo "\nbundle version number: " . $bundleVersionNumber;
     echo "\n";
-
     $appLastUpdated = Carbon::now();
-//    $pathToBuildDirectory = "builds/{$branch}";
     $pathToBuildDirectory = "builds";
-
-
-    if(! in_array($branch, ['master', 'staging'])){
-        abort(403);
-    }
 
     $iosManifestFileContents = view('ota-distribution-ios::ios-manifest')
         ->with('buildUrlRoute', $buildUrlRoute)
         ->with('publicUrl', $publicUrl)
         ->with('bundleVersionNumber', $bundleVersionNumber)
         ->render();
-
     //Render the iOS view
     $iosDownloadFileContents = view('ota-distribution-ios::ios-download')
         ->with('appLastUpdated', $appLastUpdated)
         ->with('appVersion', $bundleVersionNumber)
         ->with('urlToManifestFile', route('builds.manifest'))
         ->render();
-
     //Creates files and places them in a latest folder.
     $latestBuildPath = "{$pathToBuildDirectory}/latest";
     Storage::put("{$latestBuildPath}/manifest.plist", $iosManifestFileContents);
     Storage::put("{$latestBuildPath}/download.html", $iosDownloadFileContents);
     $file->storeAs("{$latestBuildPath}", "build.ipa");
-
     //Makes duplicate files in a separate bundle version folder for versioning.
     $bundleVersionPath = "{$pathToBuildDirectory}/{$bundleVersionNumber}";
     Storage::put("{$bundleVersionPath}/manifest.plist", $iosManifestFileContents);
@@ -57,8 +39,7 @@ Route::post('/build', function () {
     $file->storeAs($bundleVersionPath, "build.ipa");
 });
 
-
-Route::get('/build', function(){
+Route::get('/api/build', function(){
     $directory = "builds/latest";
     $buildFile = Storage::exists("{$directory}/build.ipa");
     if ($buildFile == false) {
@@ -68,8 +49,6 @@ Route::get('/build', function(){
     return Storage::get("{$directory}/download.html");
 });
 
-Route::get('/manifest', function() {
-//    $environment = env('APP_ENV');
-//    return Storage::get("builds/{$environment}/manifest.plist");
+Route::get('/api/manifest', function() {
     return Storage::get("builds/latest/manifest.plist");
 })->name("builds.manifest");
